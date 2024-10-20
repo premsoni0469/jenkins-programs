3b91e6322621de7ca2902f58af235d7d06eb2c68
Prem Soni <premsoni0469@gmail.com>
2024-10-20 13:37:04 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,5 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Addition of "+args[0]+" and "+args[1]+" is "+(Integer.parseInt(args[0])+Integer.parseInt(args[1])));
+    }
+}
---
+++ Test1.java
@@ -0,0 +1,7 @@
+public class Test1 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 15;
+        System.out.println("Addition of "+ a +" and "+ b +" is "+ (a + b));
+    }
+}
